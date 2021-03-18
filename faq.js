4b8d47b308c94f2caa4b3f3555402736f07e5d15
Vitali147 <vitali.rjabkov@gmail.com>
2021-03-18 22:57:37 +0000

    fix issue with accordion behaviour mobile design

--- faq.js
+++ faq.js
@@ -4,20 +4,25 @@ for (let i = 0; i < acc.length; i++) {
 
   acc[i].addEventListener("click", function() {
 
+
     for (let j = 0; j < acc.length; j++) {
     acc[j].classList.remove("active");
+
       if(j!=i){
         acc[j].nextElementSibling.style.display = null;
       }
     }
 
+
     this.classList.add("active");
     var panel = this.nextElementSibling;
-    if (panel.style.display){
+    if (panel.style.display === "block"){
       panel.style.display = null;
+      this.classList.remove("active");
     } else {
       panel.style.display = "block";
     }
+
 
   });
 
