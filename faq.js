80fb750987e670d49b3cd6e8e0d3bae71c5d2493
Vitali147 <vitali.rjabkov@gmail.com>
2021-03-18 00:04:13 +0000

    complete mobile first accordion design

---
+++ faq.js
@@ -0,0 +1,24 @@
+let acc = document.getElementsByClassName("question");
+
+for (let i = 0; i < acc.length; i++) {
+
+  acc[i].addEventListener("click", function() {
+
+    for (let j = 0; j < acc.length; j++) {
+    acc[j].classList.remove("active");
+      if(j!=i){
+        acc[j].nextElementSibling.style.display = null;
+      }
+    }
+
+    this.classList.add("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display){
+      panel.style.display = null;
+    } else {
+      panel.style.display = "block";
+    }
+
+  });
+
+}
